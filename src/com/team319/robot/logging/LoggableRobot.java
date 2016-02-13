15c3b016d5c36c2aaf594798ff97bac8e2b0ccc0
mwtidd <mwtidd@envisa.com>
2016-02-12 21:37:05 -0500

    Update Loggable Robot to Include Time

--- src/com/team319/robot/logging/LoggableRobot.java
+++ src/com/team319/robot/logging/LoggableRobot.java
@@ -5,8 +5,19 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
-@JsonPropertyOrder({"properties","subsystems"})
+@JsonPropertyOrder({"time","properties","subsystems"})
 public class LoggableRobot extends Loggable{
+
+	long time = 0;
+
+	public LoggableRobot(){
+		time = System.currentTimeMillis();
+	}
+
+	public long getTime() {
+		return time;
+	}
+
 	List<LoggableSubsystem> subsystems = new ArrayList<LoggableSubsystem>();
 
 	public void addSubsystem(LoggableSubsystem subsystem){
